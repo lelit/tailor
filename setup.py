b293295170426d90234ef4bc095797e1c41223f8
chris <chris@niekel.net>
2005-02-10 18:18:49 +0000

    Add setup.py for easier installation

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from distutils.core import setup
+
+setup(name='tailor',
+    version='devel-2005-02-10',
+    author='Lele Gaifax',
+    author_email='lele@nautilus.homeip.net',
+    packages=['vcpx'],
+    scripts=['tailor.py'],
+    description='a tool to migrate changesets between CVS, Subversion, and darcs repositories.',
+    long_description="""\
+This script makes it easier to keep the upstream changes merged in
+a branch of a product, storing needed information such as the upstream
+URI and revision in special properties on the branched directory.
+""",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Version Control',
+        # Don't know license
+        # Don't know the stability. Took a guess
+        ]
+    )
--- tailor.py
+++ tailor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!python
 # -*- mode: python; coding: utf-8 -*-
 # :Progetto: vcpx -- Frontend
 # :Creato:   lun 03 mag 2004 01:39:00 CEST
