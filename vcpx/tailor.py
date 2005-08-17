0f0b95f66d7310749fc0f25e1e89ce397399898f
lele <lele@nautilus.homeip.net>
2005-08-17 21:08:00 +0000

    Bump version number

--- vcpx/tailor.py
+++ vcpx/tailor.py
@@ -15,7 +15,7 @@ unversioned file named ``tailor.info`` at the root.
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.9.5'
+__version__ = '0.9.6'
 
 from optparse import OptionParser, OptionGroup, make_option
 from config import Config
