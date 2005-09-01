02b0771fe6fe9c5dc117755b18753b26a037b946
lele <lele@nautilus.homeip.net>
2005-09-01 23:12:07 +0000

    Bump version number

--- vcpx/tailor.py
+++ vcpx/tailor.py
@@ -11,7 +11,7 @@ Implement the frontend functionalities.
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.9.11'
+__version__ = '0.9.12'
 
 from optparse import OptionParser, OptionGroup, Option
 from config import Config
