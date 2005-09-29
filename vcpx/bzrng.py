9cb1724df558527ea53544a303e4f053ee52c6dc
Jelmer Vernooij <jelmer@samba.org>
2005-09-29 20:56:37 +0000

    bzrng-source-support Add initial support for Bzr as source repository.

--- vcpx/bzrng.py
+++ vcpx/bzrng.py
@@ -2,6 +2,7 @@
 # :Progetto: vcpx -- bazaar-ng support using the bzrlib instead of the frontend
 # :Creato:   Fri Aug 19 01:06:08 CEST 2005
 # :Autore:   Johan Rydberg <jrydberg@gnu.org>
+#            Jelmer Vernooij <jelmer@samba.org>
 # :Licenza:  GNU General Public License
 #
 
@@ -11,6 +12,7 @@ This module implements the backends for Bazaar-NG.
 
 __docformat__ = 'reStructuredText'
 
+from source import UpdatableSourceWorkingDir
 from target import SyncronizableTargetWorkingDir, TargetInitializationFailure
 from bzrlib.branch import Branch
 from bzrlib.errors import BzrError
@@ -20,7 +22,83 @@ import os
 import os.path
 import re
 
-class BzrngWorkingDir(SyncronizableTargetWorkingDir):
+class BzrngWorkingDir(UpdatableSourceWorkingDir, SyncronizableTargetWorkingDir):
+    ## UpdatableSourceWorkingDir
+
+    """
+    Generate changeset for the given BzrNg revision
+    """
+    def _changesetFromRevision(self, parent, revision):
+        from changes import ChangesetEntry, Changeset
+        from datetime import datetime
+        r = parent.get_revision(revision)
+            
+        deltatree = parent.get_revision_delta(parent.revision_id_to_revno(revision))
+        entries = []
+
+        for delta in deltatree.added:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.ADDED
+            entries.append(e)
+            
+        for delta in deltatree.removed:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.DELETED
+            entries.append(e)
+            
+        for delta in deltatree.renamed:
+            e = ChangesetEntry(delta[1])
+            e.action_kind = ChangesetEntry.RENAMED
+            e.old_name = delta[2]
+            entries.append(e)
+
+        for delta in deltatree.modified:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.UPDATED
+            entries.append(e)
+
+        return Changeset(r.revision_id, 
+                              datetime.fromtimestamp(r.timestamp),
+                              r.committer, 
+                              r.message,
+                              entries)
+   
+    """
+    See what other revisions exist upstream and return them
+    """
+    def _getUpstreamChangesets(self, sincerev):
+        parent = Branch.open(self.repository.repository)
+
+        revisions = self._b.missing_revisions(parent)
+
+        changesets = [self._changesetFromRevision(parent, ri) for ri in revisions]
+        
+        return changesets
+
+    """
+    Apply given remote revision to workingdir
+    """
+    def _applyChangeset(self, changeset):
+        self._b.append_revision(changeset.revision_id)
+        return [] # No conflicts for now
+
+    """
+    Initial checkout, equivalent of 'bzr branch -r ... '
+    """
+    def _checkoutUpstreamRevision(self, revision):
+        from bzrlib.clone import copy_branch
+
+        parent = Branch.open(self.repository.repository)
+
+        if revision == "INITIAL":
+            self._b = copy_branch(parent, self.basedir)
+        else:
+            self._b = copy_branch(parent, self.basedir, revision)
+
+        return self._changesetFromRevision(parent, parent.last_revision())
+
+    ## SyncronizableTargetWorkingDir
+    
     def _addPathnames(self, entries):
         # This method may get invoked several times with the same
         # entries; and Branch.add complains if a file is already
