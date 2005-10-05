f5e2110a6359c630037c039017e102995eca2cea
Jelmer Vernooij <jelmer@samba.org>
2005-10-05 12:27:13 +0000

    bzrng-replace-bzr Remove the old 'bzr' backend and replace it with the 'bzrng' (bzrlib-based) backend.

--- vcpx/bzr.py
+++ vcpx/bzr.py
@@ -1,7 +1,8 @@
 # -*- mode: python; coding: utf-8 -*-
-# :Progetto: vcpx -- bazaar-ng support
-# :Creato:   ven 20 mag 2005 08:15:02 CEST
+# :Progetto: vcpx -- bazaar-ng support using the bzrlib instead of the frontend
+# :Creato:   Fri Aug 19 01:06:08 CEST 2005
 # :Autore:   Johan Rydberg <jrydberg@gnu.org>
+#            Jelmer Vernooij <jelmer@samba.org>
 # :Licenza:  GNU General Public License
 #
 
@@ -11,115 +12,159 @@ This module implements the backends for Bazaar-NG.
 
 __docformat__ = 'reStructuredText'
 
+from source import UpdatableSourceWorkingDir
 from target import SyncronizableTargetWorkingDir, TargetInitializationFailure
-from source import ChangesetApplicationFailure
-from shwrap import ExternalCommand, ReopenableNamedTemporaryFile
+from bzrlib.branch import Branch
+from bzrlib.errors import BzrError
+from bzrlib.osutils import compact_date, rand_bytes
+from binascii import hexlify
+import os
+import os.path
+import re
 
-class BzrWorkingDir(SyncronizableTargetWorkingDir):
+class BzrWorkingDir(UpdatableSourceWorkingDir, SyncronizableTargetWorkingDir):
+    ## UpdatableSourceWorkingDir
+
+    def _changesetFromRevision(self, parent, revision):
+        """
+        Generate changeset for the given Bzr revision
+        """
+        from changes import ChangesetEntry, Changeset
+        from datetime import datetime
+        r = parent.get_revision(revision)
+            
+        deltatree = parent.get_revision_delta(parent.revision_id_to_revno(revision))
+        entries = []
+
+        for delta in deltatree.added:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.ADDED
+            entries.append(e)
+            
+        for delta in deltatree.removed:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.DELETED
+            entries.append(e)
+            
+        for delta in deltatree.renamed:
+            e = ChangesetEntry(delta[1])
+            e.action_kind = ChangesetEntry.RENAMED
+            e.old_name = delta[0]
+            entries.append(e)
+
+        for delta in deltatree.modified:
+            e = ChangesetEntry(delta[0])
+            e.action_kind = ChangesetEntry.UPDATED
+            entries.append(e)
+
+        return Changeset(r.revision_id, 
+                              datetime.fromtimestamp(r.timestamp),
+                              r.committer, 
+                              r.message,
+                              entries)
+   
+    def _getUpstreamChangesets(self, sincerev):
+        """
+        See what other revisions exist upstream and return them
+        """
+        parent = Branch.open(self.repository.repository)
+
+        revisions = self._b.missing_revisions(parent)
+
+        changesets = [self._changesetFromRevision(parent, ri) for ri in revisions]
+        
+        return changesets
+
+    def _applyChangeset(self, changeset):
+        """
+        Apply given remote revision to workingdir
+        """
+        self._b.append_revision(changeset.revision)
+        return [] # No conflicts for now
+
+    def _checkoutUpstreamRevision(self, revision):
+        """
+        Initial checkout, equivalent of 'bzr branch -r ... '
+        """
+        from bzrlib.clone import copy_branch
+
+        parent = Branch.open(self.repository.repository)
+
+        if revision == "INITIAL":
+            self._b = copy_branch(parent, self.basedir, parent.get_rev_id(1))
+        else:
+            self._b = copy_branch(parent, self.basedir, revision)
+
+        return self._changesetFromRevision(parent, parent.last_revision())
 
     ## SyncronizableTargetWorkingDir
+    
+    def _addPathnames(self, entries):
+        # This method may get invoked several times with the same
+        # entries; and Branch.add complains if a file is already
+        # versioned.  So go through the list and sort out entries that
+        # is already versioned, since there is no need to add them.
+        # Do not try to catch any errors from Branch.add, since the
+        # they are _real_ errors.
+        new_entries = []
+        for e in entries:
+            if not self._b.inventory.has_filename(e):
+                new_entries.extend([e])
 
-    def _addPathnames(self, names):
-        """
-        Add some new filesystem objects.
-        """
-
-        cmd = self.repository.command("add")
-        ExternalCommand(cwd=self.basedir, command=cmd).execute(names)
+        if len(new_entries) == 0:
+            return
+        self._b.add(new_entries)
 
     def _commit(self, date, author, patchname, changelog=None, entries=None):
-        """
-        Commit the changeset.
-        """
-
-        from locale import getpreferredencoding
-
-        encoding = ExternalCommand.FORCE_ENCODING or getpreferredencoding()
+        from time import mktime
 
         logmessage = []
         if patchname:
-            logmessage.append(patchname.encode(encoding))
+            logmessage.append(patchname)
         if changelog:
-            logmessage.append(changelog.replace('%', '%%').encode(encoding))
-        logmessage.append('')
-        logmessage.append('Original author: %s' % author.encode(encoding))
-        logmessage.append('Date: %s' % date)
+            logmessage.append(changelog)
+        if logmessage:
+            logmessage = '\n'.join(logmessage)
+        else:
+            logmessage = "Empty changelog"
+        timestamp = int(mktime(date.timetuple()))
 
-        rontf = ReopenableNamedTemporaryFile('bzr', 'tailor')
-        log = open(rontf.name, "w")
-        log.write('\n'.join(logmessage))
-        log.close()
+        # Guess sane email address
+        email = re.search("<(.*@.*)>", author)
+        if email: 
+            email = email.group(1)
+        else:
+            email = author
 
-        cmd = self.repository.command("commit", "--unchanged",
-                                      "--file", rontf.name)
-        if not entries:
-            entries = ['.']
+        revision_id = "%s-%s-%s" % (email, compact_date(timestamp),
+                                    hexlify(rand_bytes(8)))
+        self._b.commit(logmessage, committer=author,
+                       specific_files=entries, rev_id=revision_id,
+                       verbose=self.repository.project.verbose,
+                       timestamp=timestamp)
 
-        c = ExternalCommand(cwd=self.basedir, command=cmd)
-        c.execute(entries)
+    def _removePathnames(self, entries):
+        """Remove a sequence of entries"""
 
-        if c.exit_status:
-            raise ChangesetApplicationFailure("%s returned status %d" %
-                                              (str(c), c.exit_status))
+        self._b.remove(entries)
 
-    def _removePathnames(self, names):
-        """
-        Remove some filesystem objects.
-        """
+    def _renamePathname(self, oldentry, newentry):
+        """Rename an entry"""
 
-        cmd = self.repository.command("remove")
-        ExternalCommand(cwd=self.basedir, command=cmd).execute(names)
+        # bzr does the rename itself as well
+        os.rename(os.path.join(self.basedir, newentry), os.path.join(self.basedir, oldentry))
 
-    def _renamePathname(self, oldname, newname):
-        """
-        Rename a filesystem object to some other name/location.
-        """
-
-        cmd = self.repository.command("rename")
-        ExternalCommand(cwd=self.basedir, command=cmd).execute(oldname, newname)
+        self._b.rename_one(oldentry, newentry)
 
     def _prepareTargetRepository(self):
         """
         Create the base directory if it doesn't exist, and the
-        repository as well in the new working directory, executing
-        ``bzr init``.
+        repository as well in the new working directory.
         """
 
         from os.path import join, exists
 
         if not exists(join(self.basedir, self.repository.METADIR)):
-            cmd = self.repository.command("init")
-            init = ExternalCommand(cwd=self.basedir, command=cmd)
-            init.execute()
-
-            if init.exit_status:
-                raise TargetInitializationFailure(
-                    "%s returned status %s" % (str(init), init.exit_status))
-
-    def _prepareWorkingDirectory(self, source_repo):
-        """
-        Create the .bzrignore.
-        """
-
-        from os.path import join
-        from dualwd import IGNORED_METADIRS
-
-        # Create the .bzrignore file, that contains a glob per line,
-        # with all known VCs metadirs to be skipped.
-        ignore = open(join(self.basedir, '.bzrignore'), 'w')
-        ignore.write('\n'.join(['(^|/)%s($|/)' % md
-                                for md in IGNORED_METADIRS]))
-        ignore.write('\n')
-        if self.logfile.startswith(self.basedir):
-            ignore.write('^')
-            ignore.write(self.logfile[len(self.basedir)+1:])
-            ignore.write('$\n')
-        if self.state_file.filename.startswith(self.basedir):
-            sfrelname = self.state_file.filename[len(self.basedir)+1:]
-            ignore.write('^')
-            ignore.write(sfrelname)
-            ignore.write('\n')
-            ignore.write('^')
-            ignore.write(sfrelname+'.journal')
-            ignore.write('\n')
-        ignore.close()
+            self._b = Branch.initialize(self.basedir)
+        else:
+            self._b = Branch.open(self.basedir)
