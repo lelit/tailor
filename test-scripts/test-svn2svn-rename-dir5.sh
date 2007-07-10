4de21893d63e144f221ffd7e47791e4133458b0b
henry <henry@bigfoot.de>
2007-07-10 06:19:19 +0000

    [test-scripts] Use "svn update" after renaming dir, and more dirs renaming

--- test-scripts/test-svn2svn-rename-dir5.sh
+++ test-scripts/test-svn2svn-rename-dir5.sh
@@ -4,19 +4,19 @@
 # needs: test-svn2svn.include
 #
 # Test for converting revisions from Subversion to Subversion self.
-# Renameing singe directory name, without moving trees.
+# Renameing single directory name, where exist files or other dirs behind.
 #
 # ERROR:
 #  File "tailor-0.9.28/vcpx/target.py", line 528, in _renameEntries
 #    rename(absnew + '-TAILOR-HACKED-TEMP-NAME', absnew)
-
+#
 # ERROR:
 # svn commit --message "inner dir renamed: dir/subdir dir/subdir2"
 # Deleting       dir/subdir
 # svn: Commit failed (details follow):
 # svn: Out of date: '/project-a/dir/subdir' in transaction '3-1'
 #
-# Fixed by patch: svn-rename-directory-hacking2.patch
+# Errors fixed and tailor works OK now.
 
 . ./test-svn2svn.include
 subversion_setup
@@ -32,17 +32,21 @@ svn mv --force topdir/subdir/otherdir/moredir topdir/subdir/otherdir/moredir1
 svn commit --message "end of tree dir renamed: topdir/subdir/otherdir/moredir --> topdir/subdir/otherdir/moredir1"
 
 # The follow commands have a bug in SVN!
-# Can't rename after the commit the line before? Only one of these commits works. Don't know why.
+# Can't rename after the commit the line before? Only one of these commits works.
 # ERROR message from svn:
-#   svn commit --message "inner dir renamed: topdir/subdir --> topdir/subdir2 (this would fails on SVN 1.3.0)"
+#   svn commit --message "inner dir renamed: topdir/subdir --> topdir/subdir2"
 #   Deleting       dir/subdir
 #   svn: Commit failed (details follow):
 #   svn: Out of date: '/project-a/dir/subdir' in transaction '3-1'
+#
+# This can be fix by insert an extra 'update'. Hm, ok. Lets more check tailor.
 
-#svn mv topdir/subdir topdir/subdir2
-#svn commit --message "inner dir renamed: topdir/subdir --> topdir/subdir2 (this would fails on SVN 1.3.0)"
+svn update
+svn mv topdir/subdir topdir/subdir2
+svn commit --message "inner dir renamed: topdir/subdir --> topdir/subdir2"
 
-#svn mv --force topdir topdir3
-#svn commit --message "top dir renamed: topdir --> topdir3"
+svn update
+svn mv topdir topdir3
+svn commit --message "top dir renamed: topdir --> topdir3"
 
 testing_runs
