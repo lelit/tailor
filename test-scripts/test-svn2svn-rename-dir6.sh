af9b849126d008f68ef4aa76d1553ea38d18e1ec
henry <henry@bigfoot.de>
2007-07-10 06:16:00 +0000

    [test-scripts] Use "svn update" after renaming dir, no longer "--force" it

--- test-scripts/test-svn2svn-rename-dir6.sh
+++ test-scripts/test-svn2svn-rename-dir6.sh
@@ -3,7 +3,7 @@
 # File: test-svn2svn-rename-dir6.sh
 # needs: test-svn2svn.include
 #
-# Test for converting revisions from Subversion and to Subversion.
+# Test for converting revisions from Subversion to Subversion self.
 #
 # ERROR 1:
 # --------
@@ -20,27 +20,29 @@
 # creates the file as new in other path.
 # See also "test-svn2svn-#113-directory-deletion-and-rename.sh".
 #
-# File state is OK. Only the log is not complete.
+# Was an old error in log and is OK now.
 
 . ./test-svn2svn.include
 subversion_setup
 
-mkdir dir
-mkdir dir/middledir
-mkdir dir/middledir/otherdir
+mkdir -p dir/middledir/otherdir
 echo "foo" > dir/middledir/otherdir/file.txt
 svn add *
 svn commit --message "initial commit"
 
-svn rename --force dir/middledir/otherdir dir/otherdir
-svn delete --force dir/middledir
+svn rename dir/middledir/otherdir dir/otherdir
+svn delete dir/middledir
 svn commit --message "dir renamed: dir/middledir/otherdir --> dir/otherdir, remove 'middledir'"
 
 mkdir dir/innerdir
 svn add dir/innerdir
 svn commit --message "create new 'innerdir'"
 
-svn rename --force dir/otherdir dir/innerdir/otherdir
+# Is this a SVN bug? Without 'update' the next commit
+# will be copy dir/otherdir from r3 (instead from r4)
+svn update
+
+svn rename dir/otherdir dir/innerdir/otherdir
 svn commit --message "dir renamed: dir/otherdir --> dir/innerdir/otherdir"
 
 testing_runs
