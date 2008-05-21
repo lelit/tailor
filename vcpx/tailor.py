a5cd940ddd609665a6decfe2453368fa9fffe0d1
lele <lele@nautilus.homeip.net>
2008-05-21 11:00:00 +0000

    Bump up the version

--- vcpx/tailor.py
+++ vcpx/tailor.py
@@ -11,7 +11,7 @@ Implement the frontend functionalities.
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.9.32'
+__version__ = '0.9.33'
 
 from logging import getLogger
 from optparse import OptionParser, OptionGroup, Option
