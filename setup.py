4b65b6bf8f649d8ab0aabe674835e389f24b89c0
lele <lele@nautilus.homeip.net>
2008-06-01 01:46:52 +0000

    Cosmetic

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from os import walk
 try:
-	from setuptools import setup
+        from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+        from distutils.core import setup
 from vcpx.tailor import __version__ as VERSION
 
 setup(name='tailor',
@@ -18,7 +18,7 @@ setup(name='tailor',
       'version control system.',
       long_description="""\
 With its ability to "translate the history" from one VCS kind to another,
-this script makes it easier to keep the upstream changes merged in
+this tool makes it easier to keep the upstream changes merged in
 a own branch of a product.
 
 Tailor is able to fetch the history from Arch, Bazaar, CVS, Darcs, Monotone,
