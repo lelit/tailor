c5ec448461a48a97b4c982c42542be74c63aebbd
Ilia K. <mail4ilia@gmail.com>
2011-09-21 07:28:28 +0000

    git_escape_percent_sign
    ExternalCommand._execute() supports %-formatting in the command.
    This feature is not used by git repository backend, however
    if file name (or other part of a command) contains % sign
    an exception will be thrown during %-formatting in
    ExternalCommand._execute(). To prevent this, escape (i.e. double)
    all percent signs in a command.

--- vcpx/repository/git/__init__.py
+++ vcpx/repository/git/__init__.py
@@ -142,6 +142,9 @@ class GitExternalCommand(ExternalCommand):
         for each execute().
         """
 
+	#escape % signs in the command
+	command = [c.replace("%","%%") for c in command]
+
         self.repo = repo
         return ExternalCommand.__init__(self, command, cwd)
 
